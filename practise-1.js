2058f75b02883eba377fad8ab3924a3ad8c0e70c
itzSumma <itzSumma11@gmail.com>
2026-02-05 21:24:02 +0600

    first commit

---
+++ practise-1.js
@@ -0,0 +1,25 @@
+function odsAvarage(numbers){
+let odds = [];
+    for ( let number of numbers){
+      
+        if(number % 2 !== 0){ 
+           odds.push(number)
+           }
+    }
+    // console.log(odds);
+    let sum = 0;
+    for ( let number of odds){
+        sum += number;
+      
+    }
+    let count = odds.length;
+    let avarage = sum / count; 
+
+   return{
+    sum,
+    avarage
+   }
+}
+ let output=odsAvarage([42, 13, 58, 65, 81, 96, 7])
+console.log("Total sum :", output.sum,);
+  console.log  ("Avarage :", output.avarage);
